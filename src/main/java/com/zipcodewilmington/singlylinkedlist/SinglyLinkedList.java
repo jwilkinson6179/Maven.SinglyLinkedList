5d030ec17c0f876c667eb234468df28069b3acb2
James Wilkinson <jwilkinson@James-Wilkinsons-MacBook-Pro.local>
2020-03-06 20:02:40 -0500

    Copy is in an infinite loop

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,7 +1,204 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList<D> {
+    class Node<D>
+    {
+        private D value;
+        private Node<D> nextNode;
+
+        public Node(D initValue, Node<D> initNextNode)
+        {
+            this.value = initValue;
+            this.nextNode = initNextNode;
+        }
+
+        public Node<D> getNextNode()
+        {
+            return nextNode;
+        }
+
+        public void setNextNode(Node<D> pointedToNode)
+        {
+            nextNode = pointedToNode;
+        }
+
+        public D getValue()
+        {
+            return value;
+        }
+
+        public void setValue(D updatedValue)
+        {
+            value = updatedValue;
+        }
+    }
+
+    private Integer size;
+    private Node<D> head;
+    private Node<D> tail;
+
+    public SinglyLinkedList()
+    {
+        size = 0;
+        head = null;
+        tail = null;
+    }
+
+    private Node<D> getNode(Integer idx)
+    {
+        Integer currentIdx = 0;
+        Node<D> currentNode = head;
+
+        while(currentIdx < idx)
+        {
+            currentNode = currentNode.getNextNode();
+            currentIdx++;
+        }
+
+        return currentNode;
+    }
+
+    public void add(Integer idx, D value)
+    {
+        if(idx > size || idx < 0)
+        {
+            throw new IndexOutOfBoundsException();
+        }
+        else if(head == null)
+        {
+            Node<D> startingNode = new Node<D>(value, null);
+            head = startingNode;
+            tail = startingNode;
+            size++;
+        }
+        else
+        {
+            Node<D> currentNode = getNode(idx - 1);
+            Node<D> oldNextNode = currentNode.getNextNode();
+            currentNode.setNextNode(new Node<D>(value, oldNextNode));
+            size++;
+            if(tail.equals(currentNode))
+            {
+                tail = currentNode.getNextNode();
+            }
+        }
+    }
+
+    public void add(D value)
+    {
+        add(size, value);
+    }
+
+    public void remove(Integer idx)
+    {
+        if(size - 1 > idx || idx < 0)
+        {
+            throw new IndexOutOfBoundsException();
+        }
+        else if(size == 1)
+        {
+            head = null;
+            tail = null;
+            size--;
+        } else if(idx == size - 1)
+        {
+            Node<D> currentNode = getNode(idx - 1);
+            currentNode.setNextNode(null);
+            tail = currentNode;
+            size--;
+        }
+        else
+        {
+            Node<D> currentNode = getNode(idx - 1);
+            Node<D> updatedNextNode = currentNode.getNextNode().getNextNode();
+            currentNode.setNextNode(updatedNextNode);
+            size--;
+        }
+    }
+
+    public Boolean contains(D value)
+    {
+        if(head == null)
+        {
+            return false;
+        } else if(tail.getValue().equals(value))
+        {
+            return true;
+        }
+
+        Node<D> currentNode = head;
+
+        while(currentNode.getNextNode() != null)
+        {
+            if(currentNode.getValue().equals(value))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public Integer find(D value)
+    {
+        Integer currentLocation = 0;
+        Node<D> currentNode = head;
+
+        while(currentNode != null)
+        {
+            if(currentNode.getValue().equals(value))
+            {
+                return currentLocation;
+            }
+            currentLocation++;
+        }
+
+        return -1;
+    }
+
+    public Integer size()
+    {
+        return size;
+    }
+
+    public D get(Integer idx)
+    {
+        return getNode(idx).getValue();
+    }
+
+    public SinglyLinkedList<D> copy()
+    {
+        SinglyLinkedList<D> copiedList = new SinglyLinkedList<D>();
+
+        Node<D> currentNode = head;
+
+        while(currentNode != null)
+        {
+            copiedList.add(currentNode.getValue());
+        }
+
+        return copiedList;
+    }
+
+    public void sort()
+    {
+
+    }
+
+    public void reverse()
+    {
+
+    }
+
+    public SinglyLinkedList<D> slice(Integer startIdx, Integer endIdx)
+    {
+
+        return null;
+    }
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,107 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedListTest {
+public class SinglyLinkedListTest
+{
+    @Test
+    public void constructorTest()
+    {
+        SinglyLinkedList<Integer> test = new SinglyLinkedList<Integer>();
+    }
+
+    @Test
+    public void addTest()
+    {
+        Integer expected = 0;
+        String addedValue = "Hello";
+        SinglyLinkedList<String> test = new SinglyLinkedList<String>();
+
+        test.add(0, addedValue);
+        Integer actual = test.find(addedValue);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void addTwoTest()
+    {
+        Integer expected = 0;
+        Integer addedValue = 10;
+        SinglyLinkedList<Integer> test = new SinglyLinkedList<Integer>();
+
+        test.add(addedValue);
+        test.add(20);
+        test.add(30);
+        Integer actual = test.find(addedValue);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void itsATest()
+    {
+        Integer expected = 0;
+        SinglyLinkedList<Integer> test = new SinglyLinkedList<Integer>();
+
+        test.add(1);
+        test.remove(0);
+        Integer actual = test.size();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void containsTest()
+    {
+        Boolean expected = false;
+        SinglyLinkedList<Integer> test = new SinglyLinkedList<Integer>();
+
+        Boolean actual = test.contains(100);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void containsTest2()
+    {
+        Boolean expected = true;
+        SinglyLinkedList<Integer> test = new SinglyLinkedList<Integer>();
+
+        test.add(100);
+        Boolean actual = test.contains(100);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void containsTest3()
+    {
+        Boolean expected = true;
+        SinglyLinkedList<Integer> test = new SinglyLinkedList<Integer>();
+
+        test.add(100);
+        test.add(200);
+        Boolean actual = test.contains(100);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void copyTest()
+    {
+        SinglyLinkedList<String> test = new SinglyLinkedList<String>();
+        test.add("Hello");
+        test.add("James");
+        SinglyLinkedList<String> copiedTest = test.copy();
+        test.add("!");
+
+        assertFalse(copiedTest.contains("!"));
+        assertTrue(copiedTest.contains("James"));
+    }
 }
